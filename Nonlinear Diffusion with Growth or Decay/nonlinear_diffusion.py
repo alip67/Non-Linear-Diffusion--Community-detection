c22cf6d8edf99dff3bec08072729a26ac859ea2e
alip67 <alip67@users.noreply.github.com>
2020-04-07 11:05:34 -0400

    Implemnt the tree base detection for Labeling

---
+++ Nonlinear Diffusion with Growth or Decay/nonlinear_diffusion.py
@@ -0,0 +1,211 @@
+import numpy as np
+import networkx as nx
+
+
+def calculate_nonlinear_diffusion(idx_train, labels, t, h, p, L, pinvD, n, nclass, nnlinear_function):
+    preds = np.zeros((nclass, n))
+    # Start diffusion
+    labels_train = labels[idx_train]
+    for j in range(0, nclass):
+        indexes = [idx_train[i] for i, x in enumerate(labels_train) if x == j]
+        u = np.zeros(n)
+        u[indexes] = 1.0 / (len(indexes))
+        for tt in range(0, t):
+            if nnlinear_function == "power":
+                u = u - h * np.dot(L, np.power(u, p))
+                u[u < 0] = 0.0
+                u[u > 1] = 1.0
+            if nnlinear_function == "tanh":
+                u = u - h * np.dot(L, np.tanh(u))
+        train_j_class = j
+        preds[train_j_class, :] = np.maximum(preds[train_j_class, :], u)
+    return preds
+
+
+# def find_edges(G):
+#     edges = []
+#     for i in range(len(G)):
+#         for j in range(len(G[0])):
+#             if (G[i][j] == 1):
+#                  edges.append((i, j))
+#     return edges
+
+
+def CalculateDiffusionGraph(B, nclass, diffusions):
+    result = [None] * nclass
+    x = np.transpose(np.nonzero(B))
+    for i in range(0, nclass):
+        diffusion_Vector = diffusions[i, :]
+        for j in range(len(x)):
+            tempValue = np.abs(diffusion_Vector[x[j][0]] - diffusion_Vector[x[j][1]])
+            if tempValue == 0:
+                B[x[i][0]][x[i][0]] = 0.00000000000000000000000000000001
+            else:
+                B[x[i][0]][x[i][1]] = tempValue
+        result[i] = B
+    return result
+
+def CalculateSSPTree(idx_train, labels, nclass, B):
+    labels_train = labels[idx_train]
+    rank_result = [None]*nclass
+    weight_result = [None] * nclass
+    for j in range(0, nclass):
+        indexes = [idx_train[i] for i, x in enumerate(labels_train) if x == j]
+        dict_l = {}
+        dict_w = {}
+
+        G = nx.from_numpy_matrix(B[j])
+        for z in range(len(indexes)):
+            length, path = nx.single_source_dijkstra(G, indexes[z], target=None, cutoff=None, weight='weight')
+            level = {k: len(v) - 1 for k, v in path.items()}
+            if len(dict_l) == 0:
+                dict_l = level
+                dict_w=length
+            else:
+                for k, v in level.items():
+                     if k in dict_l:
+                        if dict_l[k] > v:
+                            dict_l[k] = v
+                            dict_w[k] = length[k]
+
+                        if dict_l[k] == v:
+                            if dict_w[k] > length[k]:
+                                dict_w[k]=length[k]
+
+                # dict_l = {k: min(v, dict_l[k]) for k, v in level.items() if k in dict_l}
+        rank_result[j] = dict_l
+        weight_result[j] = dict_w
+    return rank_result,weight_result
+
+def find_Min_rank(rank_dicts,weight_dicts):
+    # G = nx.from_numpy_matrix(G)
+    # nodes = list(G.nodes)
+    result = {}
+    weight={}
+
+    for i in range(0, len(rank_dicts)):
+        for k, v in rank_dicts[i].items():
+            if k in result:
+                if result[k][0] > v:
+                    result[k] = (v, i)
+                    weight[k] = weight_dicts[i][k]
+                if result[k][0]==v:
+                    if weight_dicts[i][k]< weight[k]:
+                        result[k] = (v, i)
+                        weight[k] = weight_dicts[i][k]
+            else:
+                result[k] = (v, i)
+                weight[k]=weight_dicts[i][k]
+
+    return result
+
+def Process_Result(result):
+    # sorted(results.items(), key = lambda kv: (kv[1], kv[0]))
+    rank_sorted = []
+    labels_sorted = []
+    for i in sorted(result):
+        rank_sorted.append(result[i][0])
+        labels_sorted.append(result[i][1])
+    return rank_sorted,labels_sorted
+
+
+
+
+
+
+
+
+def calculate_nonlinear_diffusion_treeBased(idx_train, labels, t, h, p, L, pinvD, n, nclass, G, nnlinear_function):
+    preds = np.zeros((nclass, n))
+    # Start diffusion
+    labels_train = labels[idx_train]
+    for j in range(0, nclass):
+        indexes = [idx_train[i] for i, x in enumerate(labels_train) if x == j]
+        u = np.zeros(n)
+        u[indexes] = 1.0 / (len(indexes))
+        for tt in range(0, t):
+            if nnlinear_function == "power":
+                u = u - h * np.dot(L, np.power(u, p))
+                u[u < 0] = 0.0
+                u[u > 1] = 1.0
+            if nnlinear_function == "tanh":
+                u = u - h * np.dot(L, np.tanh(u))
+        train_j_class = j
+        preds[train_j_class, :] = np.maximum(preds[train_j_class, :], u)
+    return preds
+
+
+def calculate_two_nonlinear_diffusions(idx_train, labels, t, h, p1, p2, LF, Ladj, sigma, pinvDF, pinvDadj, w, n, nclass,
+                                       nnlinear_function):
+    preds = np.zeros((nclass, n))
+    labels_train = labels[idx_train]
+    # Start diffusion
+    for j in range(0, nclass):
+        indexes = [idx_train[i] for i, x in enumerate(labels_train) if x == j]
+        uF = np.zeros(n)
+        uF[indexes] = 1.0 / (len(indexes))
+        uadj = np.zeros(n)
+        uadj[indexes] = 1.0 / (len(indexes))
+        print("Running nonlinear diffusion for class = %d" % (j))
+        for tt in range(0, t):
+            if nnlinear_function == "power":
+                uadj = uadj - h * np.dot(Ladj, np.power(uadj, p1)) - sigma * (uadj - uF)
+                uF = uF - h * np.dot(LF, np.power(uF, p2)) - sigma * (uF - uadj)
+                uF[uF < 0] = 0.0
+                uF[uF > 1] = 1.0
+                uadj[uadj < 0] = 0.0
+                uadj[uadj > 1] = 1.0
+            if nnlinear_function == "tanh":
+                uadj = uadj - h * np.dot(Ladj, uadj) - sigma * (uadj - uF)
+                uadj = np.tanh(uadj)
+                uF = uF - h * np.dot(LF, uF) - sigma * (uF - uadj)
+                uF = np.tanh(uF)
+        train_j_class = j
+        f = w * np.dot(pinvDF, uF) + (1 - w) * np.dot(pinvDadj, uadj)
+        preds[train_j_class, :] = np.maximum(preds[train_j_class, :], f)
+    return preds
+
+
+def choose_samples(preds, samples_no, remove_colums):
+    # Choose samples such that we preserve class distribution.
+    [nclass, n] = preds.shape
+    preds[:, remove_colums] = -1
+    sorted_index = np.argsort(preds)
+    selected_samples = np.array([])
+    for i in range(0, nclass):
+        samples_per_class = int(samples_no / nclass);
+        selected_samples_i = np.squeeze(sorted_index[i, n - samples_per_class:n])
+        selected_samples = np.unique(np.append(selected_samples, selected_samples_i)).astype(int)
+    return selected_samples
+
+
+def self_learning(idx_train, y, t, h, p1, p2, LF, Ladj, sigma, pinvDF, pinvDadj, w, n, nclass, labels, idx_test,
+                  rank_based, nnlinear_function):
+    preds = calculate_two_nonlinear_diffusions(idx_train, y, t, h, p1, p2, LF, Ladj, sigma, pinvDF, pinvDadj, w, n,
+                                               nclass, nnlinear_function)
+    if rank_based == 0:
+        pred_labels = np.argmax(preds, 0)
+    else:
+        sorted_pred = np.argsort(-preds)
+        sorted_pred_idx = np.zeros((sorted_pred.shape[0], sorted_pred.shape[1]))
+        for i in range(0, sorted_pred.shape[0]):
+            for j in range(0, sorted_pred.shape[1]):
+                sorted_pred_idx[i, sorted_pred[i][j]] = j
+        pred_labels = np.argmin(sorted_pred_idx, 0)
+    new_samples = idx_train;
+    for iter in range(5):
+        selected_samples = choose_samples(preds, 10, new_samples)
+        pred_labels[idx_train] = labels[idx_train]
+        new_samples = np.unique(np.concatenate((selected_samples, new_samples)));
+        preds = calculate_two_nonlinear_diffusions(new_samples, pred_labels, t, h, p1, p2, LF, Ladj, sigma, pinvDF,
+                                                   pinvDadj, w, n, nclass, nnlinear_function)
+        if rank_based == 0:
+            pred_labels = np.argmax(preds, 0)
+        else:
+            sorted_pred = np.argsort(-preds)
+            sorted_pred_idx = np.zeros((sorted_pred.shape[0], sorted_pred.shape[1]))
+            for i in range(0, sorted_pred.shape[0]):
+                for j in range(0, sorted_pred.shape[1]):
+                    sorted_pred_idx[i, sorted_pred[i][j]] = j
+            pred_labels = np.argmin(sorted_pred_idx, 0)
+    return preds
---
+++ Nonlinear Diffusion with Growth or Decay/train.py
@@ -0,0 +1,138 @@
+import argparse
+import numpy as np
+import random
+# import matlab.engine
+
+from utils import read_parameters, load_data, calculate_normalized_laplacian, calculate_quality_measures
+from nonlinear_diffusion import calculate_nonlinear_diffusion, calculate_two_nonlinear_diffusions, self_learning, CalculateDiffusionGraph, CalculateSSPTree, find_Min_rank, Process_Result
+import networkx as nx
+
+from sklearn.neighbors import KDTree
+import matplotlib.pyplot as plt
+
+parser = read_parameters()
+args = parser.parse_args()
+random.seed(args.seed)
+np.random.seed(args.seed)
+np.random.RandomState(args.seed)
+
+# Load data
+adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, labels, graph = load_data(args.dataset,
+                                                                                                  args.normalize)
+print("Done loading data")
+idx_train = [i for i, x in enumerate(train_mask) if x]
+idx_val = [i for i, x in enumerate(val_mask) if x]
+idx_test = [i for i, x in enumerate(test_mask) if x]
+n = adj.shape[0]
+
+if args.generate_knn == 1:
+    dense_features = features
+    Tree = KDTree(dense_features)
+    dists, idxs = Tree.query(dense_features, k=args.knighbors_number)
+    dists = np.exp(-(dists ** 2 / (2 * args.rad ** 2)))
+    G = np.zeros((n, n))
+    G[np.tile(range(0, n), args.knighbors_number), np.reshape(idxs, n * args.knighbors_number, 1)] = np.reshape(dists,
+                                                                                                                n * args.knighbors_number,
+                                                                                                                1)
+    # Zero diagonal elements.
+    for i in range(0, n):
+        G[i, i] = 0.0
+    # Make G symmtric!
+    Gtranspose = np.transpose(G)
+    G = G + Gtranspose - np.sqrt(G * Gtranspose)
+    print("Done building knn graph")
+    dsqrt = np.diag(np.sqrt(G.sum(axis=0)))
+    x, y = np.nonzero(dsqrt)
+    nnz = x.shape[0]
+    for i in range(0, nnz):
+        if dsqrt[x[i], y[i]] != 0:
+            dsqrt[x[i], y[i]] = 1.0 / dsqrt[x[i], y[i]];
+    G = np.dot(np.dot(dsqrt, G), dsqrt);
+    np.save('G_' + args.dataset + '_k_' + str(args.knighbors_number) + '_rad_' + str(args.rad) + '.npy', G);
+else:
+    print("Done loading knn graph")
+    G = np.load('G_' + args.dataset + '_k_' + str(args.knighbors_number) + '_rad_' + str(args.rad) + '.npy');
+
+[n, nclass] = labels.shape
+if args.use_two_diffusions == 0:
+    B = adj.A
+    B = B.astype(float)
+    G = args.w * G + (1.0 - args.w) * adj.A
+    # GG = np.dot(G, G)
+    # G = G + GG
+    load_data = False
+    y = np.argmax(labels, 1)
+    if load_data == True:
+        L = np.load('L_' + args.dataset + 'Normalized laplacian' + '.npy')
+        pinvD = np.load('pinVD_' + args.dataset + 'PinVD' + '.npy')
+        preds = np.load('Preds_' + args.dataset + 'Preds' + '.npy')
+    else:
+        L, pinvD = calculate_normalized_laplacian(G)
+        preds = calculate_nonlinear_diffusion(idx_train, y, args.t, args.h, args.p1, L, pinvD, n, nclass, args.function)
+        np.save('L_' + args.dataset + 'Normalized laplacian' + '.npy', L)
+        np.save('pinVD_' + args.dataset + 'PinVD' + '.npy', pinvD)
+        np.save('Preds_' + args.dataset + 'Preds' + '.npy', preds)
+
+    C = CalculateDiffusionGraph(B, nclass,preds)
+    SSSP_Rank,SSSP_Weight = CalculateSSPTree(idx_train, y, nclass,C)
+    DicOfRanks = find_Min_rank(SSSP_Rank,SSSP_Weight)
+    result = Process_Result(DicOfRanks)
+
+else:
+    LF, pinvDF = calculate_normalized_laplacian(G)
+    Ladj, pinvDadj = calculate_normalized_laplacian(1.0 * adj.A)
+    y = np.argmax(labels, 1)
+    if args.self_learning == 0:
+        preds = calculate_two_nonlinear_diffusions(idx_train, y, args.t, args.h, args.p1, args.p2, LF, Ladj, args.sigma,
+                                                   pinvDF, pinvDadj, args.w, n, nclass, args.function)
+    else:
+        preds = self_learning(idx_train, y, args.t, args.h, args.p1, args.p2, LF, Ladj, args.sigma, pinvDF, pinvDadj,
+                              args.w, n, nclass, y, idx_test, args.rank_based, args.function)
+    C = CalculateDiffusionGraph((adj.A).astype(float), nclass,preds)
+    SSSP_Rank,SSSP_Weight = CalculateSSPTree(idx_train, y, nclass, C)
+    DicOfRanks = find_Min_rank(SSSP_Rank,SSSP_Weight)
+    result = Process_Result(DicOfRanks)
+print("Done calculating the diffusions")
+
+if args.rank_based == 0:
+    # Get predicted labels for each data point.
+    pred_labels = np.argmax(preds, 0)
+    # Report accuracy on test set.
+    y = np.argmax(labels[idx_val, :], 1)
+    pred_labels_test = pred_labels[idx_test]
+    y = np.argmax(labels[idx_test, :], 1)
+    calculate_quality_measures(y, pred_labels_test)
+else:
+    sorted_pred = np.argsort(-preds)
+    sorted_pred_idx = np.zeros((sorted_pred.shape[0], sorted_pred.shape[1]))
+    for i in range(0, sorted_pred.shape[0]):
+        for j in range(0, sorted_pred.shape[1]):
+            sorted_pred_idx[i, sorted_pred[i][j]] = j
+    pred_labels = np.argmin(sorted_pred_idx, 0)
+    # Report accuracy on test set.
+    y = np.argmax(labels[idx_val, :], 1)
+    pred_labels_test = pred_labels[idx_test]
+    y = np.argmax(labels[idx_test, :], 1)
+    calculate_quality_measures(y, pred_labels_test)
+
+
+    labelSorted = np.load('labelsSortedlabelsSorted' + '.npy')
+    newMethod_labels_test = labelSorted[idx_test]
+    rankedSorted = np.load('rankSorted_rankSorted' + '.npy')
+    newMethod_ranked_test = rankedSorted[idx_test]
+    calculate_quality_measures(y, newMethod_labels_test)
+
+    X=np.arange(0.0, len(newMethod_ranked_test), 1)
+
+
+    # plt.plot(X, newMethod_ranked_test)  # Plotting the line plot
+    # plt.plot(X, pred_labels_test)  # Plotting the line plot
+    plt.scatter(X[0:100], newMethod_ranked_test[0:100], c='b', marker='o')
+    plt.scatter(X[0:100], pred_labels_test[0:100], c='r', marker='o')
+    # Labeling the X-axis
+    plt.xlabel('X-axis')
+    # Labeling the Y-axis
+    plt.ylabel('Y-axis')
+    # Give a title to the graph
+    plt.title('Simple Line Plot')
+    plt.show()
